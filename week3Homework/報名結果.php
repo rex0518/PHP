3943d8f4e98af8b57d6dde3cd758642ca982d145
Rex <tcmt5685@gmail.com>
2025-03-14 11:01:00 +0800

    Added week2Homework and week3Class, deleted Week2

---
+++ week3Homework/報名結果.php
@@ -0,0 +1,9 @@
+<?php
+    $name = $_POST["name"];
+    $grade = $_POST["grade"];
+    $phoneNumber = $_POST["phoneNumber"];
+
+    echo "你的姓名是: " . $name . "<br>";
+    echo "你的年級是: " . $grade . "<br>";
+    echo "你的電話號碼是: " . $phoneNumber . "<br>";
+?>
