4da8e577d56d76ea35f24d8a833c3157c6489d27
Rex <tcmt5685@gmail.com>
2025-05-02 15:04:06 +0800

    更新

---
+++ week12Class/form.php
@@ -0,0 +1,8 @@
+<script src="https://challenges.cloudflare.com/turnstile/v0/api.js" defer></script>
+
+<form action="show.php" method="POST">
+  Username: <input type="text" name="uName"><br>
+  Userpassword: <input type="password" name="uPwd"><br>
+  <div class="cf-turnstile" data-sitekey="0x4AAAAAABY3YgoCMK3U9yFH"></div>
+  <input type="submit" value="Submit">
+</form>
